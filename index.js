a66bbbae229714e68c8097d353f87fde134dcdaf
William Chan <wc2184@nyu.edu>
2022-07-04 01:27:44 -0400

    .

--- index.js
+++ index.js
@@ -34,42 +34,46 @@ let PORT = process.env.PORT || 5000;
 // });
 
 app.get("/download", (req, res) => {
-  try {
-    console.log("download ran");
-    let title;
-    let videolink = req.query.link;
-    console.log(videolink, "this is the link");
-
-    let file = fs.createWriteStream("thevideo.mp4");
-
-    ytdl(videolink).pipe(file);
-    ytdl.getInfo(videolink).then((data) => {
-      console.log(data.videoDetails.title);
-      title = data.videoDetails.title;
-    });
-    //   http://localhost:5000/download?link=https://www.youtube.com/watch?v=AuO0Y8Iwq0E
-
-    console.log(downloadsFolder());
-    location = downloadsFolder();
-    file.on("finish", () => {
-      // fs.writeFile(location, file, function callback(err) {
-      //   if (err) throw err;
-      //   console.log(downloaded);
-      // });
-      // res.send("Downloaded!");
-      title.replace(" ", "_");
-      res.attachment(`${title}.mp4`);
-      res.set("Access-Control-Expose-Headers", "Content-Disposition");
-      res.set("Content-Disposition", `attachment;  ${title}.mp4`);
-      console.log(JSON.stringify(res.headers));
-      console.log(res, "response");
-      console.log(res.getHeaders(), "response");
-      res.download("thevideo.mp4", `${title}.mp4`);
-    });
-  } catch (err) {
+  console.log("download ran");
+  let title;
+  let videolink = req.query.link;
+  if (
+    !/^(?:https?:\/\/)?(?:(?:www\.)?youtube.com\/watch\?v=|youtu.be\/)(\w+)$/.test(
+      videolink
+    )
+  ) {
     console.log(error);
     res.send(500, { ok: false });
   }
+
+  console.log(videolink, "this is the link");
+
+  let file = fs.createWriteStream("thevideo.mp4");
+
+  ytdl(videolink).pipe(file);
+  ytdl.getInfo(videolink).then((data) => {
+    console.log(data.videoDetails.title);
+    title = data.videoDetails.title;
+  });
+  //   http://localhost:5000/download?link=https://www.youtube.com/watch?v=AuO0Y8Iwq0E
+
+  console.log(downloadsFolder());
+  location = downloadsFolder();
+  file.on("finish", () => {
+    // fs.writeFile(location, file, function callback(err) {
+    //   if (err) throw err;
+    //   console.log(downloaded);
+    // });
+    // res.send("Downloaded!");
+    title.replace(" ", "_");
+    res.attachment(`${title}.mp4`);
+    res.set("Access-Control-Expose-Headers", "Content-Disposition");
+    res.set("Content-Disposition", `attachment;  ${title}.mp4`);
+    console.log(JSON.stringify(res.headers));
+    console.log(res, "response");
+    console.log(res.getHeaders(), "response");
+    res.download("thevideo.mp4", `${title}.mp4`);
+  });
 });
 
 app.listen(PORT, () => {
