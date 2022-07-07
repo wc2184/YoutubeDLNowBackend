4e976f9ed0d7cb129bd898de26126aa1a70dc2d6
wc2184 <68214261+wc2184@users.noreply.github.com>
2022-07-07 19:57:59 -0400

    .

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const ytdl = require("ytdl-core");
 const downloadsFolder = require("downloads-folder");
 var cors = require("cors");
+const ytpl = require("ytpl");
+
 const app = express();
 
 app.on("uncaughtException", function (req, res, route, err) {
@@ -32,6 +34,12 @@ let PORT = process.env.PORT || 5000;
 //   const user = req.query.user;
 //   res.send(user + "is what you typed");
 // });
+function youtube_parser(url) {
+  var regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  var match = url.match(regExp);
+  return match && match[7].length == 11 ? match[7] : false;
+}
 
 app.get("/download", (req, res) => {
   console.log("download ran");
@@ -44,8 +52,8 @@ app.get("/download", (req, res) => {
   }
 
   console.log(videolink, "this is the link");
-
-  let file = fs.createWriteStream("thevideo.mp4");
+  let filenames = youtube_parser(videolink) + ".mp4";
+  let file = fs.createWriteStream(filenames);
   file.on("error", function (err) {
     console.log(err);
     file.end();
@@ -84,12 +92,34 @@ app.get("/download", (req, res) => {
     res.attachment(`${title}.mp4`);
     res.set("Access-Control-Expose-Headers", "Content-Disposition");
     res.set("Content-Disposition", `attachment;  ${title}.mp4`);
-    console.log(JSON.stringify(res.headers));
-    console.log(res, "response");
+    // console.log(JSON.stringify(res.headers));
+    // console.log(res, "response");
     console.log(res.getHeaders(), "response");
-    res.download("thevideo.mp4", `${title}.mp4`);
+    res.download(filenames, `${title}.mp4`);
   });
   //   res.sendStatus(500);
+});
+// just make another api request with the playlist download, use react front end to parse the playlist
+// put everything into a file, and zip/compress that file
+
+app.get("/playlist", (req, res) => {
+  console.log(req.query.link);
+  res.send(req.query.link);
+  let playlist = req.query.link.split("list=")[1];
+  console.log(playlist ? "yes" : "undefinedddd daddy");
+  if (playlist) return res.end();
+  console.log(playlist, "playlist");
+  ytpl("PLD7SPvDoEddZUrho5ynsBfaI7nqhaNN5c")
+    .then((res) => {
+      // console.log(res.items);
+      console.log(res.items.length);
+      res.items.forEach((item) => {
+        console.log(item.title);
+        console.log(item.shortUrl);
+        console.log("-------");
+      });
+    })
+    .catch((err) => console.log(err, "err playlist"));
 });
 
 app.listen(PORT, () => {
---
+++ test.js
@@ -0,0 +1,94 @@
+{
+    id: 'PLJdEWbB6MKlyZqSqnJyujNplBtNCQO0xw',
+    url: 'https://www.youtube.com/playlist?list=PLJdEWbB6MKlyZqSqnJyujNplBtNCQO0xw',
+    title: 'one',
+    estimatedItemCount: 3,
+    views: 0,
+    thumbnails: [
+      {
+        url: 'https://i.ytimg.com/vi/GLs2xxM0e78/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDHdOjiiTQ0w3DfIKZ_u4AldKxbJw',
+        width: 336,
+        height: 188
+      },
+      {
+        url: 'https://i.ytimg.com/vi/GLs2xxM0e78/hqdefault.jpg?sqp=-oaymwEXCPYBEIoBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAksZmeUxh0mnjCuLbL9-DkyPalSA',
+        width: 246,
+        height: 138
+      },
+      {
+        url: 'https://i.ytimg.com/vi/GLs2xxM0e78/hqdefault.jpg?sqp=-oaymwEWCMQBEG5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLDsuXyYiFk5Sh_FGZP-ey8piKp85g',
+        width: 196,
+        height: 110
+      },
+      {
+        url: 'https://i.ytimg.com/vi/GLs2xxM0e78/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&rs=AOn4CLA-lZS-w6wLGsayyO-IOzh6JnghBw',
+        width: 168,
+        height: 94
+      }
+    ],
+    bestThumbnail: {
+      url: 'https://i.ytimg.com/vi/GLs2xxM0e78/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDHdOjiiTQ0w3DfIKZ_u4AldKxbJw',
+      width: 336,
+      height: 188
+    },
+    lastUpdated: 'Updated today',
+    description: '',
+    visibility: 'unlisted',
+    author: {
+      name: 'Mustafa Khurram',
+      url: 'https://www.youtube.com/c/MustafaKhurram',
+      avatars: [ [Object], [Object], [Object] ],
+      bestAvatar: {
+        url: 'https://yt3.ggpht.com/ytc/AKedOLSu4y4nkmPe4gZknNQMkrxdiz8wsv76JPhWIMSgDg=s176-c-k-c0x00ffffff-no-rj',
+        width: 176,
+        height: 176
+      },
+      channelID: 'UCZVQEbUO9zRr2Nxe3vfSUNQ'
+    },
+    items: [
+      {
+        title: 'Amsterdam | Official Trailer | 20th Century Studios',
+        index: 1,
+        id: 'GLs2xxM0e78',
+        shortUrl: 'https://www.youtube.com/watch?v=GLs2xxM0e78',
+        url: 'https://www.youtube.com/watch?v=GLs2xxM0e78&list=PLJdEWbB6MKlyZqSqnJyujNplBtNCQO0xw&index=1',
+        author: [Object],
+        thumbnails: [Array],
+        bestThumbnail: [Object],
+        isLive: false,
+        duration: '2:39',
+        durationSec: 159,
+        isPlayable: true
+      },
+      {
+        title: 'Lithium is dangerous',
+        index: 2,
+        id: 'yGDkiUAwxRs',
+        shortUrl: 'https://www.youtube.com/watch?v=yGDkiUAwxRs',
+        url: 'https://www.youtube.com/watch?v=yGDkiUAwxRs&list=PLJdEWbB6MKlyZqSqnJyujNplBtNCQO0xw&index=2',
+        author: [Object],
+        thumbnails: [Array],
+        bestThumbnail: [Object],
+        isLive: false,
+        duration: '1:00',
+        durationSec: 60,
+        isPlayable: true
+      },
+      {
+        title: 'Melis - Waves',
+        index: 3,
+        id: 'jpAlF_sL6j0',
+        shortUrl: 'https://www.youtube.com/watch?v=jpAlF_sL6j0',
+        url: 'https://www.youtube.com/watch?v=jpAlF_sL6j0&list=PLJdEWbB6MKlyZqSqnJyujNplBtNCQO0xw&index=3',
+        author: [Object],
+        thumbnails: [Array],
+        bestThumbnail: [Object],
+        isLive: false,
+        duration: '4:09',
+        durationSec: 249,
+        isPlayable: true
+      }
+    ],
+    continuation: null
+  }
+  
