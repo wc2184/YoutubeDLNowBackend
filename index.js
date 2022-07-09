a1c8ca0a8574b4581eddbc89eeee4e27ff496e0a
wc2184 <68214261+wc2184@users.noreply.github.com>
2022-07-08 20:21:45 -0400

    .

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.get("/playlist/:type", (req, res) => {
         // .pipe(file); // if video then add this
         let start = Date.now();
         if (type == "audio") {
-          console.log("yes its audio");
+          console.log("yes its audio", __dirname);
           ffmpeg(yt)
             .audioBitrate(128)
             .save(`${__dirname}/playlists/${playlist}/${item.title}.mp3`)
